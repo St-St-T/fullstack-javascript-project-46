647c57ee02c503a44f7d1bc72201fd2db156f264
Stanislav Stolyarov <stst09stanislav@yandex.ru>
2025-12-03 20:49:19 +0700

    Шаг 3 - Чтение и парсинг файлов

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander'
+import fs from 'fs'
 
 
 program
@@ -9,5 +10,40 @@ program
   .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
   .argument('<filepath2>')
+  .action((path1, path2) => {
 
+    var files;
+
+    if (!fs.existsSync(path1) && !fs.existsSync(path2)){
+      files = 0;
+    }
+    else if (!fs.existsSync(path1)){
+      files = 1;
+    }
+    else if (!fs.existsSync(path2)){
+      files = 2;
+    }
+    else{
+      files = 3;
+    }
+
+    switch (files){
+      case 0:
+        console.log("Files not found");
+        break;
+      case 1:
+        console.log("File 1 was not found");
+        break;
+      case 2:
+        console.log("File 2 was not found");
+        break;
+      case 3:
+        const data1 = JSON.parse(fs.readFileSync(path1));
+        const data2 = JSON.parse(fs.readFileSync(path2));
+        console.log(data1);
+        console.log(data2);
+        break;
+    }
+
+  });
 program.parse()
